ff3a3f7d00ddd35410fcaa496351353a354ffb18
Anoop Manikanta <anoop.ms@surya-soft.com>
2022-02-08 18:39:45 +0530

    Completed quiz app

---
+++ Quiz2/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  Quiz2
+//
+//  Created by Anoop Subramani on 08/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of India?",
+        "What is the recent Playstation game available on PC?"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "New Delhi",
+        "God of War"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton){
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+}
+
